7de88faab650e63a37fc628c85eb836421667ef7
Antoine Neuenschwander <antoine.neuenschwander@switch.ch>
2018-02-05 14:13:36 +0100

    added more pools

--- filter.py
+++ filter.py
@@ -2,19 +2,33 @@
 
 import csv
 import dns.resolver
+import json
 
 rules = {}
+reverse_rules = {}
 with open('pools.csv', 'rb') as csvfile:
     poolreader = csv.reader(csvfile, delimiter=',')
     for row in poolreader:
-        answers = dns.resolver.query(row[0], 'A')
+        domain = row[0]
+        port = row[1]
+        currency = row[2]
+        answers = dns.resolver.query(domain, 'A')
         ips = [str(ip) for ip in answers]
-        if not rules.get(row[2]):
-            rules[row[2]] = []
-        rules[row[2]].append('(dst ip in [%s] and proto tcp and port %s)' % (', '.join(ips), row[1]))
+        if not rules.get(currency):
+            rules[currency] = []
+        rules[currency].append('(dst ip in [%s] and proto tcp and port %s)' % (', '.join(ips), port))
+        for ip in answers:
+            key = "%s:%s" % (ip, port)
+            reverse_rules[key] = "currency: %s, pool: %s:%s" % (currency, domain, port)
  
 for k in rules.keys():
     print '=== ' + k
     print " or\n".join(rules[k])
     print
+
+print
+print "-------------"
+print
+
+print json.dumps(reverse_rules, indent=4, separators=(',', ': '))
 
