c0aba9c32425ef5ff050191a00a171b02aa3668c
Antoine Neuenschwander <antoine.neuenschwander@switch.ch>
2018-07-05 00:22:15 +0200

    Read database url from environment

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
  * https://medium.com/the-andela-way/deploying-a-python-flask-app-to-heroku-41250bda27d0
  * https://github.com/jokamjohn/bucket_api_heroku/blob/f3f9380c069b5a0a5d5116f2bd0ae1882c90ec69/app/models.py
  * https://hub.docker.com/_/postgres/
+ * https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-xviii-deployment-on-heroku
--- app/__init__.py
+++ app/__init__.py
@@ -9,10 +9,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+
 # create and configure the app
 app = Flask(__name__, instance_relative_config=True)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://cryptoioc:password1@localhost/cryptoioc'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ('DATABASE_URL')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
